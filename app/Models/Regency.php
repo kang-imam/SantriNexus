9351dc6707706f8cc191dc34fd5858e3eeee0077
kang-imam <imamulmutaqinalhanif@gmail.com>
2026-07-15 00:24:53 +0700

    penyiapan model & migrasi daerah

---
+++ app/Models/Regency.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Regency extends Model
+{
+    protected $fillable = [
+        'province_id',
+        'code',
+        'name',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function province(): BelongsTo
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    public function districts(): HasMany
+    {
+        return $this->hasMany(District::class);
+    }
+}
